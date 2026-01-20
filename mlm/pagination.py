b71f6c7872c6580035854f601350e14dabdb0ba1
kodamharish <kodamharish12@gmail.com>
2026-01-20 10:12:47 +0530

    updated pagination

--- mlm/pagination.py
+++ mlm/pagination.py
@@ -1,6 +1,6 @@
 from rest_framework.pagination import PageNumberPagination
 
 class GlobalPagination(PageNumberPagination):
-    page_size = 20
+    page_size = 1000
     page_size_query_param = 'page_size'
-    max_page_size = 100
+    max_page_size = 2000
